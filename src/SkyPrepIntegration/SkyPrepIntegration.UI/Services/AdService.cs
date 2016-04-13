43d5d34ca3a98163a7637f4b9ad5e8f650d2d4e1
arash <arash@ARASH-I3>
2016-04-13 17:34:15 -0400

    Added nuget package manager

--- src/SkyPrepIntegration/SkyPrepIntegration.UI/Services/AdService.cs
+++ src/SkyPrepIntegration/SkyPrepIntegration.UI/Services/AdService.cs
@@ -1,9 +1,35 @@
 ﻿namespace SkyPrepIntegration.UI.Services
 {
+    using System;
     using System.DirectoryServices.AccountManagement;
+    using System.Web;
 
     public class AdService
+
+
     {
+
+        public static UserPrincipal getCurrentUser()
+        {
+            try
+            {
+                return UserPrincipal.Current;
+                
+
+            } catch (Exception e)
+            {
+                try
+                {
+                    var domain = new PrincipalContext(ContextType.Domain);
+                    var name = HttpContext.Current.User.Identity.Name;
+                    return UserPrincipal.FindByIdentity(domain, name);
+                }   catch (Exception ee)
+                {
+                    
+                }
+            }
+            return null;
+        } 
         public static string GetFirstName()
         {
             return UserPrincipal.Current.GivenName;
